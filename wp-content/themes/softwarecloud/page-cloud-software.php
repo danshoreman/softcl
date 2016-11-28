08509c6dcc03302efa3dd24eafbc3ddea64e431e
Dan Shoreman <danshoreman@Dans-MBP.home>
2016-11-28 11:28:57 +0000

    Adding you get a lot panel

--- wp-content/themes/softwarecloud/page-cloud-software.php
+++ wp-content/themes/softwarecloud/page-cloud-software.php
@@ -6,34 +6,30 @@
 	
 	<?php include 'partials/page-intro.php'; ?>
 	
-	<div class="graduates-panel">
+	<div class="what-you-get-panel">
 		<div class="row">
-			<div class="small-12 medium-6 columns">
-				<h2>You get a lot.</h2>
+			<div class="small-12 columns">
 				
-				<div class="row">
-					<div class="small-12 medium-6 large-3 columns">
-						<p>Office 365 <br>included</p>
-					</div>
-					<div class="small-12 medium-6 large-3 columns">
-						<p>8x8 <br>Gartner-leading<br> telephony</p>
-					</div>
-					<div class="small-12 medium-6 large-3 columns">
-						<p>Choose<br>the apps<br> you want</p>
-					</div>
-					<div class="small-12 medium-6 large-3 columns">
-						<p>128 bit<br>encryption</p>
-					</div>
-					<div class="small-12 medium-6 large-3 columns">
-						<p>40GB* <br>user storage</p>
-					</div>
-					<div class="small-12 medium-6 large-3 columns">
-						<p>Back-end<br>finally ditch that old<br>below-desk server</p>
-					</div>
-					<div class="small-12 medium-6 large-3 columns">
-						<p>Anti-malware<br>keep users<br>out of trouble</p>
-					</div>
-				</div>
+				<h2><?php the_field('cloud_offered_title'); ?></h2>
+				
+				<?php if( have_rows('cloud_tools') ): ?>
+
+					<ul class="what-you-get">
+				
+					<?php while( have_rows('cloud_tools') ): the_row(); 
+				
+						$item = get_sub_field('item'); ?>
+				
+						<li class="wyg-item">
+							<?php echo $item; ?>
+						</li>
+				
+					<?php endwhile; ?>
+				
+					</ul>
+				
+				<?php endif; ?>
+				
 			</div>
 		</div>
 	</div>
